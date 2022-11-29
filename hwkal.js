382c7032f1a298a59db149d2629dfa67df12946f
kingknown <118806215+kingknown@users.noreply.github.com>
2022-11-29 14:35:13 +0700

    Update hwkal.js

--- hwkal.js
+++ hwkal.js
@@ -22,20 +22,20 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
 global.youtube = 'https://youtube.com/c/HwModsWa857' // ubah aia
-global.ig = '@hikal857' // ubah aja
-global.email = 'denyp857@gmail.com'
+global.ig = '@znxph' // ubah aja
+global.email = 'znxph960@gmail.com'
 global.region = 'indonesia'
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'Dani Maker☆' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Dani Maker☆' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'Dani Maker☆' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'King ph☆' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'King Ph☆' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'King Ph☆' //ubah jadi nama mu, note tanda ' gausah di hapus!
 //=================================================//
 // Other
-global.owner = ['6281251549899','6281348106274'] // ubah aja pake nomor lu
-global.premium = ['6281251549899','6281348106274'] // ubah aja pake nomor lu
-global.packname = 'Dani Maker☆' // ubah aja
-global.ownerr = ['Dani Maker☆'] // ubaha aja
-global.author = 'Dani Maker☆' //ubah aja
+global.owner = ['62857973750143','62857973750143'] // ubah aja pake nomor lu
+global.premium = ['62857973750143','62857973750143'] // ubah aja pake nomor lu
+global.packname = 'King Ph☆' // ubah aja
+global.ownerr = ['King Ph☆'] // ubaha aja
+global.author = 'King Ph☆' //ubah aja
 global.sessionName = './sessionye/haikal' //Gausaj Di Ubah
 global.prefa = ['','!','.',',','🐤','🗿'] // Gausah Mendingan 
 global.sp = '⭔' // Gausah Juga
